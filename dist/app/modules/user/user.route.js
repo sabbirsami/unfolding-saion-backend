d810b758554aee22bd0f374901d08c0fce279150
Sabbir Mohammad Sami <smd71430@gmail.com>
2024-12-23 17:00:33 +0600

    feat: handle error

--- dist/app/modules/user/user.route.js
+++ dist/app/modules/user/user.route.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserRoute = void 0;
 const express_1 = require("express");
 const user_controller_1 = require("./user.controller");
+const validateRequest_1 = require("../../middlewares/validateRequest");
+const user_validation_1 = require("./user.validation");
 const router = (0, express_1.Router)();
 /**
  * @swagger
@@ -65,5 +67,5 @@ const router = (0, express_1.Router)();
  *                 stack:
  *                   type: string
  */
-router.post('/register', user_controller_1.UserController.createUser);
+router.post('/register', (0, validateRequest_1.validateRequest)(user_validation_1.UserValidation.createUserValidationSchema), user_controller_1.UserController.createUser);
 exports.UserRoute = router;
